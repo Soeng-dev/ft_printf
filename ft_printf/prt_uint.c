b5137d0a724ac1937a5bcee2f348a889a78c28d3
Soengyong Kim <soekim@c1r7s4.42seoul.kr>
2021-02-22 19:47:54 +0900

    norm and # + flag done, debug gonna start

--- ft_printf/prt_uint.c
+++ ft_printf/prt_uint.c
@@ -6,7 +6,7 @@
 /*   By: soekim <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/21 19:54:55 by soekim            #+#    #+#             */
-/*   Updated: 2021/02/22 19:44:39 by soekim           ###   ########.fr       */
+/*   Updated: 2021/02/22 19:46:44 by soekim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,60 +22,6 @@ int		get_total_uintlen(int digitlen, t_tag tag)
 	if (tag.zero_flag)
 		total_intlen = ftmax(tag.width, total_intlen);
 	return (total_intlen);
-}
-
-int		prt_convint(long long int i, t_tag tag, int digitlen, int total_intlen)
-{
-	int	prtlen;
-
-	prtlen = 0;
-	if (tag.zero_flag)
-	{
-		if (i < 0)
-			prtlen += iter_write('-', 1);
-		else if (tag.sign_flag)
-			prtlen += iter_write(tag.sign_flag, 1);
-		prtlen += iter_write('0', tag.width - digitlen - prtlen);
-	}
-	else if (tag.aligned == RIGHT)
-		prtlen += iter_write(' ', tag.width - total_intlen);
-	if (tag.zero_flag == FALSE)
-	{
-		if (i < 0)
-			prtlen += iter_write('-', 1);
-		else if (tag.sign_flag)
-			prtlen += iter_write(tag.sign_flag, 1);
-	}
-	prtlen += iter_write('0', tag.precision - digitlen);
-	if (digitlen)
-		ft_putnbr_fd((i < 0) ? (-i) : (i), 1);
-	prtlen += digitlen;
-	if (tag.aligned == LEFT)
-		prtlen += iter_write(' ', tag.width - prtlen);
-	return (prtlen);
-}
-
-int		prt_int(va_list param, t_tag tag)
-{
-	long long int	i;
-	long long int	temp;
-	int				total_intlen;
-	int				digitlen;
-
-	i = get_intarg(param, tag.memlen);
-	temp = (i < 0) ? -i : i;
-	digitlen = 0;
-	while (temp > 0)
-	{
-		temp /= 10;
-		++digitlen;
-	}
-	if (tag.precision != UNSET)
-		tag.zero_flag = FALSE;
-	if (tag.precision == UNSET && digitlen == 0)
-		++digitlen;
-	total_intlen = get_total_intlen(digitlen, (i >= 0) ? (1) : (-1), tag);
-	return (prt_convint(i, tag, digitlen, total_intlen));
 }
 
 int		prt_convuint(unsigned long long i, t_tag tag,\
@@ -114,6 +60,6 @@ int		prt_uint(va_list param, t_tag tag)
 	}
 	if (tag.precision == UNSET && digitlen == 0)
 		++digitlen;
-	total_intlen = get_total_intlen(digitlen, 1, tag);
+	total_intlen = get_total_uintlen(digitlen, tag);
 	return (prt_convuint(i, tag, digitlen, total_intlen));
 }
